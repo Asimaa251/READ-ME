63abb281aa9fa63ae78d6abeedd00d1ab42b0bc2
Asimaa251 <nrlasmrrh@gmail.com>
2025-04-17 07:29:19 +0700

    Menambahkan warna.js

---
+++ warna.js/index.js
@@ -0,0 +1,6 @@
+// File: warna.js
+import chalk from "chalk";('chalk');
+
+console.log(chalk.blue('Teks biru!'));
+console.log(chalk.green('Teks hijau!'));
+console.log(chalk.red.bold('Teks merah tebal!'));
